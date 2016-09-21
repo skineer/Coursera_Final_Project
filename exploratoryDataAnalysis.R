a72f043fc96d4dba61c1d52324c680dd2d0c8d62
Pedroso <Renato.Pedroso@CAPCO.COM>
2016-09-21 08:29:15 -0300

    probability of terms

--- exploratoryDataAnalysis.R
+++ exploratoryDataAnalysis.R
@@ -7,7 +7,7 @@
 ####################################################################################
 
 library("tm")
-
+setwd("C:\\Users\\lc43922\\Coursera_Final_Project\\final")
 blog_us <- readLines("en_US/en_US.blogs.txt", warn = FALSE)
 news_us <- readLines("en_US/en_US.news.txt", warn = FALSE)
 twitter_us <- readLines("en_US/en_US.twitter.txt", warn = FALSE)
@@ -26,3 +26,4 @@ twitterWordCount <- sum(sapply(gregexpr("\\W+", twitter_us), length) + 1)
 exploratoryAnalysis <- data.frame(File = c("Blogs","News","Twitter"),
                                   Line_Count = c(blogsLineCount, newsLineCount, twitterLineCount),
                                   Word_Count = c(blogsWordCount, newsWordCount, twitterWordCount))
+
--- ngramModel.R
+++ ngramModel.R
@@ -38,19 +38,30 @@ nGramMaker3 <- function(charVectorized, n = 3){
   tokenizer <- NGramTokenizer(charVectorized, Weka_control(min = n, max = n))
 }
 
-wordCountVector<- function(vector, searchOn){
-  # this function receive a vector of words or ngrams and count the ocurrencies on the "search on"
-  # vector --> a vector of words or ngrams (already tokenized)
-  # searchOn --> data frame, matrix, large character types to search the ocurrencies of words
-  # return   --> return a data frame with 2 columns. Word and Ocurrencies
+wordCountVector<- function(vector, searchOn, gramNumber, gramFull){
+  # this function receive a vector of words and count the ocurrencies on the "search on"
+  # also calculates the probability, using Markov Chains with add-1 smooth
+  # to predict words the backoff model needs to be implemented as well
+  # based on https://www.r-bloggers.com/natural-language-processing-what-would-shakespeare-say/
+  # vector     --> a vector of words or ngrams (already tokenized)
+  # searchOn   --> data frame, matrix, large character types to search the ocurrencies of words
+  # gramNumber --> size of the gram (1 = unigram, 2 = bigram, 3 = trigram)
+  # gramFull   --> original constructed ngram to get the V for smoothing
+  # return      --> return a data frame with 3 columns. Word, Ocurrencies, probability
   wordCountDF <- data.frame(word=character(length(vector)), 
-                            ocurrencies = integer(length(vector)), stringsAsFactors = FALSE)
+                            ocurrencies = integer(length(vector)),
+                            probability = numeric(length(vector)), stringsAsFactors = FALSE)
+  V = gramFull$nrow
   for (i in 1:length(vector)){
     wordVector = vector[i]
     wordCountDF$word[i] <- wordVector
     search <- paste("\\",wordVector,"\\>", sep = '')
     numOcurrences <- length(grep(paste("\\<",wordVector,"\\>", sep = ''), searchOn))
     wordCountDF$ocurrencies[i] <- numOcurrences
+    if (gramNumber == 1){
+      wordCountDF$probability[i] <- wordCountDF$ocurrencies[i] / V
+    }
+    
   }
   return(wordCountDF)
 } 
@@ -93,6 +104,8 @@ gram1Filtered <- findFreqTerms(gram1,lowfreq = 50)
 gram2Filtered <- findFreqTerms(gram2,lowfreq = 50)
 gram3Filtered <- findFreqTerms(gram3,lowfreq = 50)
 
-wordCountGram1 <- wordCountVector(gram1Filtered, sampleEnglishOnly)
-wordCountGram2 <- wordCountVector(gram2Filtered, sampleEnglishOnly)
-wordCountGram3 <- wordCountVector(gram3Filtered, sampleEnglishOnly)
+wordCountProbGram1 <- wordCountVector(gram1Filtered, sampleEnglishOnly, gramNumber = 1, gramFull = gram1)
+wordCountProbGram2 <- wordCountVector(gram2Filtered, sampleEnglishOnly, gramNumber = 2, gramFull = gram2)
+wordCountProbGram3 <- wordCountVector(gram3Filtered, sampleEnglishOnly, gramNumber = 3, gramFull = gram3)
+
+
